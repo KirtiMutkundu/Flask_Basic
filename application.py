7968693adfe92a38b686d2c2ce6cc225f0f4f901
KirtiMutkundu <kirtimutkundureddy2007@gmail.com>
2024-06-22 13:42:35 +0530

    first commit

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 FAI=Flask(__name__)
 @FAI.route('/errorhari')
 def errorhari():
     return 'Errorhari Is Best'
-FAI.run (debug=True)
+
+@FAI.route('/hai')
+def hai():
+    return render_template('hai.html',name='kirti',age=21)
+FAI.run (debug=True,host='192.168.16.13',port=8000)
